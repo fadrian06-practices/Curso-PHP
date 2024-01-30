f7cff164951a07006b2947e30678caff4392ce4a
fadrian06 <franyeradriansanchez@gmail.com>
2024-01-30 11:48:39 -0400

    Comentarios y variables

--- index.php
+++ index.php
@@ -6,9 +6,15 @@
   </head>
   <body>
     <?php
-      print 'Bienvenidos al curso de PHP <br />';
+      print 'Bienvenidos al curso de PHP <br />'; // imprime y hace un salto de línea (<br />)
       print 'Hola alumnos <br />';
       print 'Hola el próximo video';
+
+      /* Texto
+      dividido
+      en
+      muchas
+      lineas. */
     ?>
   </body>
 </html>
---
+++ variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+  </head>
+  <body>
+    <?php
+      // $nombre = "Franyer";
+      $nombre = 'Franyer';
+      // $nombre = 'Adrián';
+      $edad = 22;
+
+      // print $nombre;
+      // print 'El nombre del usuario es: ' . $nombre;
+      // print "El nombre del usuario es: $nombre";
+      // print "El nombre es $nombre y tiene $edad años";
+
+      // echo $nombre;
+      // echo 'El nombre es ' . $nombre . ' y tiene ' . $edad . ' años';
+      echo "El nombre es $nombre y tiene $edad años";
+      // echo $nombre, $edad;
+      // print $nombre, $edad;
+    ?>
+  </body>
+</html>
